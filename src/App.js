219556068756f87fe2726aba6e233200c84f7089
Jonathan Dempsey <jonathanedempsey@gmail.com>
2021-10-14 16:51:42 +0100

    Convert vanilla React code to JSX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom"
 import Pet from "./Pet";
 
@@ -7,16 +7,30 @@ import Pet from "./Pet";
 // it is required that components are capitalised, specifically when using JSX.
 // JSX will assume anything captalised is something you created, while something lowercase,
 // like "div", is an existing element
+
+// vanilla react code
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         [
+//             React.createElement("h1", {}, "Adopt Me!"),
+//             React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
+//             React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+//             React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" }),
+//         ]
+//     );
+// };
+
+// JSX code
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-            React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-            React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" }),
-        ]
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+            <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+        </div>
     );
 };
 
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+// vanilla react code
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h2", {}, props.name),
+//         React.createElement("h3", {}, props.animal),
+//         React.createElement("h3", {}, props.breed),
+//     ]);
+// };
+
+// JSX code
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ]);
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <h3>{props.animal}</h3>
+            <h3>{props.breed}</h3>
+        </div>
+    );
 };
 
 export default Pet;
